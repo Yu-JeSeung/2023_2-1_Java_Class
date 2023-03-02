bd18120c87d0e0918b325b93e1dd378e93beef15
yjs <wptmd97@naver.com>
2023-03-02 14:41:07 +0900

    0302.1

---
+++ src/C0302/HelloWorld.java
@@ -0,0 +1,11 @@
+package C0302;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("안녕하세요 이번학기도 잘 부탁드립니다.");
+        // 대문자는 대부분 클래스다
+        // 괄호가 있는 애들은 메소드
+        // .. 사이에 있는 애들은 필드
+    }
+}
